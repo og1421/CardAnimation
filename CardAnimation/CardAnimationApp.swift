af618d632a2a440389a11b9243a18ef06e85b07c
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 17:31:14 -0300

    Initial Commit

---
+++ CardAnimation/CardAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAnimationApp.swift
+//  CardAnimation
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
